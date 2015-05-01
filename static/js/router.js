497847c416dc0ae7841c2c48c67bbae52851ee35
zhengkun <zhengkun@parllay.cn>
2015-05-01 16:13:23 +0800

    update static files

--- static/js/router.js
+++ static/js/router.js
@@ -24,37 +24,105 @@ var router={
     },
     index:function(){
         this.body.load('../../application/views/home.html',function(){
-
-        });
-        this.body.find('.main_left li').click(function(){
-            var id=$(this).attr('extra-data');
-            $.ajax({
-                type:'GET',
-                url:'json/data'+id+'.json',
-                dataType:'json',
-                success:function(data){
-                    console.log(data);
-                    if(data.error){
+            this.body.find('.main_left li').click(function(){
+                var id=$(this).attr('extra-data');
+                $.ajax({
+                    type:'GET',
+                    url:'json/data'+id+'.json',
+                    dataType:'json',
+                    success:function(data){
+                        console.log(data);
+                        if(data.error){
+                            alert('内部错误');
+                            return
+                        }
+                        if(data){
+                            data=eval('('+data+')');
+                            $('#detail_name').text(data.name);
+                            $('#detail_size').text(data.size);
+                            $('#detail_cost').text(data.cost);
+                            $('#detail_pic').attr('src',data.url)
+                        }
+                    },
+                    error:function(){
                         alert('内部错误');
-                        return
                     }
-                    if(data){
-                        data=eval('('+data+')');
-                        $('#detail_name').text(data.name);
-                        $('#detail_size').text(data.size);
-                        $('#detail_cost').text(data.cost);
-                        $('#detail_pic').attr('src',data.url)
-                    }
-                },
-                error:function(){
-                    alert('内部错误');
-                }
-            })
-        });
-        this.background2()
+                })
+            });
+            this.background2()
+        }.bind(this))
+
     },
     cart:function(){
-        var html=$('')
+        this.body.load('../../application/views/cart.html',function(){
+            var allData=[];
+            $('#cart_list li').each(function(){
+                var target=$(this);
+                $(this).find('.plus').click(function(){
+                    $(this).siblings('.count').text(parseInt($(this).siblings('.count').text())+1);
+                    selectProduct(target)
+                });
+                $(this).find('.reduce').click(function(){
+                    if(parseInt($(this).siblings('.count').text())>1){
+                        $(this).siblings('.count').text(parseInt($(this).siblings('.count').text())-1);
+                        selectProduct(target)
+                    }
+                });
+                $(this).find('[name=item]').change(function(){
+                    selectProduct(target)
+                })
+            });
+
+            $('#addAll').change(function(){
+                var state=$(this).attr('checked');
+                if(state){
+                    $('#cart_list [name=item]').attr('checked',true);
+                }else{
+                    $('#cart_list [name=item]').attr('checked',false);
+                }
+                $('#cart_list li').each(function(){
+                    selectProduct($(this))
+                });
+            });
+
+
+            function selectProduct(ele){
+                if(ele.find('[name=item]').attr('checked')) {
+                    var result = _.find(allData, function (re) {
+                        return re.id == ele.attr('productId')
+                    });
+                    var count = ele.find('.count').text();
+
+                    if (result) {
+                        result.count = count;
+                        result.credit = ele.attr('credit') * count;
+                    } else {
+                        allData.push({
+                            id: ele.attr('productId'),
+                            name: ele.attr('product'),
+                            size: ele.attr('size'),
+                            count: count,
+                            credit: ele.attr('credit') * count
+                        })
+                    }
+                }else{
+                    var result = _.find(allData, function (re) {
+                        return re.id == ele.attr('productId')
+                    });
+                    if(result){
+                        var index=_.indexOf(allData,result);
+                        allData.splice(index,1)
+                    }
+                }
+                var total=0;
+                console.log(allData);
+                allData.forEach(function(i){
+                    total=total+parseInt(i.credit)
+                });
+                $('#totalCredit').text(total)
+            }
+            this.background1()
+        }.bind(this))
     }
 }
 
