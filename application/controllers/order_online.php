c20eaf602a7366620d73efe74156ca25dc0f5ffb
liutao <liutao@parllay.cn>
2015-05-01 11:05:21 +0800

    update codes

--- application/controllers/order_online.php
+++ application/controllers/order_online.php
@@ -8,4 +8,13 @@
 
 class Order extends CI_Controller {
 
+    public function getBasicInfo($id){
+        $this->customer_model->get_customer_info($id);
+    }
+
+
+    public function addCart($product_id,$spec_id,$product_num){
+        $current_customer_id = $_SESSION["customer_id"];
+        $this->order_online_model->home_get_userinfo();
+    }
 } 
--- application/models/product_model.php
+++ application/models/product_model.php
@@ -5,3 +5,23 @@
  * Date: 5/1/15
  * Time: 9:56 AM
  */
+
+class Product_Model extends CI_Model {
+
+    /**
+     * drop product from shopping cart
+     * @param $customer_id
+     * @param $product_id
+     * @param $spec_id
+     */
+    function drop_cart($customer_id,$product_id,$spec_id){
+        $query = array(
+            'customer_id' => $customer_id,
+            'product_id' => $product_id,
+            'spec_id' => $spec_id
+        );
+        $this->db->where($query);
+        $this->db->delete('rtm_shopping_cart');
+    }
+
+}
