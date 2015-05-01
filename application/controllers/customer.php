317b0e7af0e173527a93bef1dd66124770bc8ae5
caozhenhuamail <caozhenhuamail@gmail.com>
2015-05-01 22:24:47 +0800

    add customer info controller operator

--- application/controllers/customer.php
+++ application/controllers/customer.php
@@ -8,16 +8,48 @@
 
 class Customer extends CI_Controller {
 
+    public function index($id, $wechat_id=""){
+        $data['customer_info'] = $this->customer_model->get_customer_by_customer_id($id);
+        $type = "add";
+        if(is_array($data) && $data !=null){
+            $type = "update";
+        }
+        $data['type'] = $type;
+        $data['wechat_id'] = $wechat_id;
+        $this->load->view('shopping/info.php', $data);
+    }
+
     public function get($id){
         $this->customer_model->get_customer_info($id);
     }
 
-    public function add($name,$address,$phone,$email,$wechat_id){
-        $this->customer_model->add_customer_info($name,$address,$phone,$email,$wechat_id);
+    public function add(){
+        $name = $_POST['name'];
+        $birthday = $_POST['birthday'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $wechat_id = $_POST["wechat_id"];
+        $result = $this->customer_model->add_customer_info($name,$birthday,$phone,$email,$wechat_id);
+        if($result){
+            return json_decode(array("success" => true, "message" => $result));
+        } else {
+            return json_decode(array("success" => false, "message" => $result));
+        }
     }
 
-    public function update($id,$name,$address,$phone,$email,$wechat_id){
-        $this->customer_model->update_customer_info($id,$name,$address,$phone,$email,$wechat_id);
+    public function update(){
+        $name = $_POST['name'];
+        $birthday = $_POST['birthday'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $wechat_id = $_POST["wechat_id"];
+        $id = $_POST["id"];
+        $result = $this->customer_model->update_customer_info($id,$name,$birthday,$phone,$email,$wechat_id);
+        if($result){
+            return json_decode(array("success" => true, "message" => $result));
+        } else {
+            return json_decode(array("success" => false, "message" => $result));
+        }
     }
 
     public function addDelivery($receiver_name,$receiver_phone,$receiver_province,$receiver_city,$receiver_region,$receiver_address,$is_default){
