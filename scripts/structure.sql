7501ba7f079b35cc0eff301896748801b10361dc
Steve Gao <fu@2token.com>
2015-04-30 21:15:16 -0700

    correct column name for rtm_product_specification table

--- scripts/structure.sql
+++ scripts/structure.sql
@@ -229,16 +229,16 @@ DROP TABLE IF EXISTS `rtm_product_specification`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rtm_product_specification` (
   `product_id` int(11) DEFAULT NULL,
-  `sepc_id` varchar(4) NOT NULL,
+  `spec_id` varchar(4) NOT NULL,
   `score` int(11) NOT NULL,
   `stock_num` int(11) NOT NULL COMMENT '库存数量',
   `exchange_num` int(11) NOT NULL COMMENT '可用于积分对换的数量',
   `is_for_exchange` tinyint(1) NOT NULL COMMENT '是否用于积分对换，有些商品是不能用于积分对换的\n积分商城中显示的商品应该使用该字段为true',
   `status` tinyint(1) DEFAULT NULL COMMENT '商品状态，比如上架，下架之类',
-  PRIMARY KEY (`sepc_id`),
+  PRIMARY KEY (`spec_id`),
   KEY `fk_rtm_product_specification_1_idx` (`product_id`),
   CONSTRAINT `fk_rtm_product_specification_product_id` FOREIGN KEY (`product_id`) REFERENCES `rtm_product_info` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_rtm_product_specification_sepc_id` FOREIGN KEY (`sepc_id`) REFERENCES `rtm_global_specification` (`spec_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_rtm_product_specification_spec_id` FOREIGN KEY (`spec_id`) REFERENCES `rtm_global_specification` (`spec_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
