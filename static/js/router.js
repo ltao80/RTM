94544dc7bf04638a0f2ac79d14ce78fca0fc04b4
zhengkun <zhengkun@parllay.cn>
2015-05-02 09:41:18 +0800

    router code change

--- static/js/router.js
+++ static/js/router.js
@@ -1,4 +1,6 @@
 
+var url=window.location.href.split('/');
+var openId=url[url.length-1];
 
 var router={
     wrapper:$('#wrapper'),
@@ -32,7 +34,7 @@ var router={
     },
     /****************************主 页*****************************/
     index:function(){
-        router.body.load('/shopping/home/123456',function(){
+        router.body.load('/shopping/home/'+openId,function(){
             router.header.empty();
             $('.home_button').attr('extra-data',router.body.find('.main_left li:eq(0)').attr('extra-data'));
             router.body.find('.main_left li').click(function(){
