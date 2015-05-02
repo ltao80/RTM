1c7609b5a35f1b13e798bfcf42df7e808d7dd79d
Ye Sun <sunye@parllay.cn>
2015-05-02 20:22:46 +0800

    update text

--- application/views/shopping/home.php
+++ application/views/shopping/home.php
@@ -5,7 +5,7 @@
     <div class="right_info">
         <img src="../../static/images/logo.png" />
         <h1>尊敬的人头马会员: <?php echo isset($customer_list['name']) ? $customer_list['name'] : "匿名用户"?></h1>
-        <p>您目前享有积分：<?php echo isset($customer_list['total_score']) ?$customer_list['total_score']:0?>分 </p>
+        <p>您目前享有积分：<?php echo isset($customer_list['total_score']) ?$customer_list['total_score']:0?>积分 </p>
     </div>
     <div class="main_left">
         <ul>
@@ -24,7 +24,7 @@
         <div style="width:100%; height:1px; overflow:hidden; clear: both"></div>
         <h1 class="detail_name"><span id="detail_name"><?php echo $promation_list[0]['name']?></span></h1>
         <p class="detail_size" id="detail_size"><?php echo $promation_list[0]['spec_name']?></p>
-        <p class="detail_cost">所需积分:<span id="detail_cost"><?php echo $promation_list[0]['score']?>分</span></p>
+        <p class="detail_cost">所需积分:<span id="detail_cost"><?php echo $promation_list[0]['score']?>积分</span></p>
         <button class="home_button">立即兑换</button>
         <div class="hr"></div>
     </div>
