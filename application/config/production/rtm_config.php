ffd35eced2404b070f41c391ff77d9198b30ad4d
caozhenhuamail <caozhenhuamail@gmail.com>
2015-05-02 08:20:57 +0800

    add customer info controller operator

--- application/config/production/rtm_config.php
+++ application/config/production/rtm_config.php
@@ -5,6 +5,7 @@
 * To change the template for this generated file go to
 * Window - Preferences - PHPeclipse - PHP - Code Templates
 */
-$config['wxapiurl'] = '';
-
+$config['platId'] = "Q3SXLR95OG";
+$config['getTokenUrl'] = "http://wxapi.parllay.cn/social/token/get?platId=Q3SXLR95OG";
+$config['pgDomain'] = "http://rtm-ppe.parllay.cn/";
 ?>
