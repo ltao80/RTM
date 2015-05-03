aa2e38a00874686bbd467c6f3e765f444af015f0
zhengkun <zhengkun@parllay.cn>
2015-05-03 14:37:22 +0800

    router code change

--- application/views/shopping/edit-delivery.php
+++ application/views/shopping/edit-delivery.php
@@ -6,11 +6,11 @@
             <p>手机号码</p>
             <input type="tel" name="info_tel" value="<?php if(isset($customer_info['receiver_phone'])){echo $customer_info['receiver_phone'];}?>" class="required info_input" placeholder="手机号码" />
             <p>省</p>
-            <input type="text" name="info_addr" value="<?php if(isset($customer_info['receiver_province'])){echo $customer_info['receiver_province'];}?>" class="required info_input" placeholder="省" />
+            <input type="text" name="info_province" value="<?php if(isset($customer_info['receiver_province'])){echo $customer_info['receiver_province'];}?>" class="required info_input" placeholder="省" />
             <p>市</p>
-            <input type="text" name="info_addr" value="<?php if(isset($customer_info['receiver_city'])){echo $customer_info['receiver_city'];}?>" class="required info_input" placeholder="市" />
+            <input type="text" name="info_city" value="<?php if(isset($customer_info['receiver_city'])){echo $customer_info['receiver_city'];}?>" class="required info_input" placeholder="市" />
             <p>区</p>
-            <input type="text" name="info_addr" value="<?php if(isset($customer_info['receiver_region'])){echo $customer_info['receiver_region'];}?>" class="required info_input" placeholder="区" />
+            <input type="text" name="info_region" value="<?php if(isset($customer_info['receiver_region'])){echo $customer_info['receiver_region'];}?>" class="required info_input" placeholder="区" />
             <p>详细地址</p>
             <input type="text" name="info_addr_detail" value="<?php if(isset($customer_info['receiver_address'])){echo $customer_info['receiver_address'];}?>" class="required info_input" placeholder="详细地址ַ" />
 
