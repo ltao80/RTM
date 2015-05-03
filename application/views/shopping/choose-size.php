483dd622fff247ac56d5929feba3b56c3bbf7c9a
liutao <liutao@parllay.cn>
2015-05-03 10:53:18 +0800

    update codes

--- application/views/shopping/choose-size.php
+++ application/views/shopping/choose-size.php
@@ -2,11 +2,13 @@
     <div class="confirm">
         <p class="confirm_title">请选择数量<span id="close">×</span></p>
         <div class="confirm_main">
-            <h1>人头马禧钻特优香槟干邑</h1>
-            <p>所需积分 <span>500</span> 积分</p>
+            <h1><?php if(isset($product_info['name'])){echo $product_info['name'];}?></h1>
+            <p>所需积分 <span></span> 积分</p>
             <div class="choose_size">
                 <?php foreach($product_spec_list as $product_spec){?>
-                <div><?php if(isset($product_spec['spec_name'])){echo $product_spec['spec_name'];}?></div>
+                    <div spec_id="<?php if(isset($product_spec['spec_id'])){echo $product_spec['spec_id'];}?>"></div>
+                    <div score="<?php if(isset($product_spec['score'])){echo $product_spec['score'];}?>"></div>
+                    <div><?php if(isset($product_spec['spec_name'])){echo $product_spec['spec_name'];}?></div>
                 <?php } ?>
             </div>
             <div class="confirm_count">
@@ -18,7 +20,7 @@
                 <a href="javascript:void(0)" class="detail_btn" id="submit">确认提交</a>
             </div>
             <form id="form">
-                <input type="hidden" name="name" id="name" value="人头马禧钻特优香槟干邑" />
+                <input type="hidden" name="name" id="name" value="<?php if(isset($product_info['name'])){echo $product_info['name'];}?>" />
                 <input type="hidden" name="count" id="count" value="1" />
             </form>
         </div>
