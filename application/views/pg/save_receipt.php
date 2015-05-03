a41171be1b3db4171d057554e1d5feb5dec01b75
zhengkun <zhengkun@parllay.cn>
2015-05-03 08:19:03 +0800

    router code change

--- application/views/pg/save_receipt.php
+++ application/views/pg/save_receipt.php
@@ -5,7 +5,7 @@
 <div class="main">
     <div class="save-receipt-form signin_form">
         <p>记录购物小票编号</p>
-        <input type="text" class="receipt-id info_input" input="receipt_id" placeholder="请输入购物小票编号">
+        <input type="text" class="receipt-id info_input" id="receipt_id" placeholder="请输入购物小票编号">
         <a href="javascript:void(0)" class="save-receipt detail_btn" id="submit">确认</a>
     </div>
 </div>
