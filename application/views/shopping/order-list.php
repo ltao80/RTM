e438229e70d8ef317e00edb66be8a87938bcc19e
liutao <liutao@parllay.cn>
2015-05-04 09:01:13 +0800

    update codes

--- application/views/shopping/order-list.php
+++ application/views/shopping/order-list.php
@@ -3,13 +3,14 @@
         <?php foreach($order_list as $order_info){?>
         <li>
             <h1><?php if(isset($order_info['order_datetime'])){echo $order_info['order_datetime'];}?></h1>
-            <?php foreach($order_info['detail'] as $product_info){?>
+            <?php foreach($order_info['detail'] as $order_detail){?>
             <div class="oders_main">
-                <div class="confirm_img"><img src="/static/images/<?php if(isset($product_info['image_url'])){echo
-                $product_info['image_url'];}?>" /></div>
-                <p><?php if(isset($product_info['description'])){echo $product_info['description'];}?></p>
-                <h2>规格：<?php if(isset($product_info['spec_name'])){echo $product_info['spec_name'];}?></h2>
-                <h3><?php if(isset($product_info['score'])){echo $product_info['score'];}?>积分</h3>
+                <div class="confirm_img"><img src="/static/images/<?php if(isset($order_detail['image_url'])){echo
+                    $order_detail['image_url'];}?>" /></div>
+                <p><?php if(isset($order_detail['description'])){echo $order_detail['description'];}?></p>
+                <h2>规格：<?php if(isset($product_info['spec_name'])){echo $order_detail['spec_name'];}?></h2>
+                <h3><?php if(isset($order_detail['score'])){echo $order_detail['score'];}?>积分</h3>
+                <h3>x<?php if(isset($order_detail['product_num'])){echo $order_detail['product_num'];}?></h3>
             </div>
             <?php } ?>
             <p>
--- application/views/shopping/score-detail.php
+++ application/views/shopping/score-detail.php
@@ -6,5 +6,5 @@
         <li><?php if(isset($order_info['name'])){echo $order_info['name'];}?> <?php if(isset($order_info['spec_name'])){echo $order_info['spec_name'];}?> x<?php if(isset($order_info['product_num'])){echo $order_info['product_num'];}?><span><?php if(isset($order_info['score'])){echo $order_info['score'];}?></span></li>
         <?php } ?>
     </ul>
-    <div class="detail_info">获得积分：<span><?php if(isset($score_info['total_score'])){echo $score_info['total_score'];}?></span></div>
+    <div class="detail_info">获得积分：<span><?php if(isset($score_info['order_type'])){if($score_info['order_type'] == "1"){echo "-";}else{echo "+";}}?><?php if(isset($score_info['total_score'])){echo $score_info['total_score'];}?></span></div>
 </div>
