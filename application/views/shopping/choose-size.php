4084947062bff2dc5a7a7c6cc3c61717c829f96f
zhengkun <zhengkun@parllay.cn>
2015-05-04 10:39:33 +0800

    router code change

--- application/views/shopping/choose-size.php
+++ application/views/shopping/choose-size.php
@@ -6,14 +6,18 @@
             <p>所需积分 <span id="total_score"></span> 积分</p>
             <div ></div>
             <div class="choose_size">
+                <h2 style="display: inline-block; color: #fff">规格：</h2>
                 <?php foreach($product_spec_list as $product_spec){?>
                     <div spec_id="<?php if(isset($product_spec['spec_id'])){echo $product_spec['spec_id'];}?>" size="<?php if(isset($product_spec['spec_name'])){echo $product_spec['spec_name'];}?>" score="<?php if(isset($product_spec['score'])){echo $product_spec['score'];}?>"><?php if(isset($product_spec['spec_name'])){echo $product_spec['spec_name'];}?></div>
                 <?php } ?>
             </div>
-            <div class="confirm_count">
-                <div class="reduce">-</div>
-                <p class="count">1</p>
-                <div class="plus">+</div>
+            <div style=" margin-bottom:50px">
+                <h2 style="display: inline-block; color: #fff">数量：</h2>
+                <div class="confirm_count">
+                    <div class="reduce">-</div>
+                    <p class="count">1</p>
+                    <div class="plus">+</div>
+                </div>
             </div>
             <div class="product_foot">
                 <a href="javascript:void(0)" class="detail_btn" id="submit">确认提交</a>
