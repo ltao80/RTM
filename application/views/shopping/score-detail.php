b8f006970dd5f3e4fa95bf030f724e225058f561
liutao <liutao@parllay.cn>
2015-05-08 18:26:48 +0800

    update codes

--- application/views/shopping/score-detail.php
+++ application/views/shopping/score-detail.php
@@ -1,7 +1,8 @@
 <div class="main">
     <ul class="detail_list">
 
-        <li><?php if(isset($score_info['order_datetime'])){echo $score_info['order_datetime'];}?><span><?php if(isset($score_info['store_name'])){echo $score_info['store_name'];}?></span></li>
+        <li><?php if(isset($score_info['order_datetime'])){echo date('Y-m-d H:i:s',strtotime
+            ($score_info['order_datetime']));}?><span><?php if(isset($score_info['store_name'])){echo $score_info['store_name'];}?></span></li>
         <?php foreach($score_info['detail'] as $order_info){?>
         <li><?php if(isset($order_info['name'])){echo $order_info['name'];}?> <?php if(isset($order_info['spec_name'])){echo $order_info['spec_name'];}?> x<?php if(isset($order_info['product_num'])){echo $order_info['product_num'];}?><span><?php if(isset($order_info['score'])){echo $order_info['score'];}?></span></li>
         <?php } ?>
