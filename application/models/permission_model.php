6baf4200fd7f73ea190197659f93b2e5b3028041
liutao <ltao80@126.com>
2015-05-23 19:09:23 +0800

    update codes

--- application/models/permission_model.php
+++ application/models/permission_model.php
@@ -7,5 +7,111 @@
  */
 
 class permission_model {
+    public function save_role($role_id,$role_name,$description,$permission_codes){
+        if(!isset($role_id)){
+            $this->db->trans_start();
+            $data = array(
+                'role_name' => $role_name,
+                'description' => $description,
+            );
+            $this->db->insert("lp_role_info",$data);
+            $permissions = array();
+            foreach($permission_codes as $permission_code){
+                $data = array(
+                    "permission_code" => $permission_code,
+                    "role_id" => $role_id
+                );
+                $permissions[] = $data;
+            }
+            $this->db->insert_batch("lp_role_permission",$permissions);
+            $this->db->trans_complete();
+        }else{
+            $this->db->trans_start();
+            $data = array(
+                'role_name' => $role_name,
+                'description' => $description,
+            );
+            $this->db->where("id",$role_id);
+            $this->db->update("lp_role_info",$data);
 
+            $this->db->where("role_id",$role_id);
+            $this->db->delete("lp_role_permission");
+
+            $permissions = array();
+            foreach($permission_codes as $permission_code){
+                $data = array(
+                    "permission_code" => $permission_code,
+                    "role_id" => $role_id
+                );
+                $permissions[] = $data;
+            }
+            $this->db->insert_batch("lp_role_permission",$permissions);
+            $this->db->trans_complete();
+        }
+    }
+
+    public function delete_role($role_id){
+        $this->db->where("role_id",$role_id);
+        $this->db->delete("lp_role_info");
+        $rows = $this->db->affected_rows();
+        if($rows <= 0)
+            throw new RuntimeException("Failed to delete role");
+    }
+
+    public function query_role_list($role_name,$pageIndex,$pageSize){
+        if(isset($province)){
+            $this->db->where("name","match",$role_name);
+        }
+        if(isset($city)){
+            $this->db->where("b.city",$city);
+        }
+        if(isset($prefix)){
+            $this->db->where("a.name",'match',$prefix);
+        }
+        if(isset($status)){
+            $this->db->where("a.status",$status);
+        }
+        $this->db->select("a.id, a.name, a.phone, a.email, a.status, b.province, b.city, b.store_name");
+        $this->db->from("lp_promotion_info a");
+        $this->db->join("lp_global_store b","b.store_id = a.store_id");
+        $this->db->order_by("a.last_login","desc");
+        $this->db->limit($pageIndex,$pageSize);
+        return $this->db->get()->result_array();
+    }
+
+    public function get_permissions_by_role_ids($role_ids){
+        $this->db->where_in("role_id",$role_ids);
+        $this->db->select('permission_code');
+        $this->db->from("lp_role_permission");
+        $this->db->distinct();
+        return $this->db->get()->result_array();
+    }
+
+    public function get_permission_menu_by_codes($permission_codes){
+        $this->db->where_in("role_id",$permission_codes);
+        $this->db->select("menu_name,order_number,parent_id");
+        $this->db->order_by("order_number");
+        $this->db->from("lp_permission_menu");
+        $menus = $this->db->get()->result_array();
+        $main_menus = array();
+        $sub_menus = array();
+        foreach($menus as $menu){
+            if(empty($menu['parent_id'])){
+                $main_menus[] = $menu;
+            }else{
+                if(empty($sub_menus[$menu['parent_id']])){
+                    $sub_menus[$menu['parent_id']] = $menu;
+                }
+            }
+        }
+        $result = array();
+        foreach($main_menus as $main_menu){
+            if(!empty($sub_menus[$main_menu['id']])){
+                $main_menu['sub_menu'] = $sub_menus[$main_menu['id']];
+                $result[] = $main_menu;
+            }
+        }
+
+        return $result;
+    }
 }
