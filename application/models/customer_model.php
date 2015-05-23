d0f60518d32119010be22fa1a8a424105f13ba5d
liutao <ltao80@126.com>
2015-05-23 21:33:42 +0800

    update codes

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -288,66 +288,32 @@ class Customer_Model extends CI_Model {
         }
     }
 
+
     /**
-     * @param $openId
-     * @return int  1 : 不存在，进入首次验证页面， 2： 登录， 3： 当天已经登录，无需登录
+     * @param $startDate
+     * @param $endDate
+     * @param $province
+     * @param $city
+     * @param $page_index
+     * @param $page_size
+     * @return mixed
      */
-    function verify($openId) {
-        $query = $this->db->query("SELECT * FROM lp_promotion_info WHERE wechat_id = '$openId'");
-
-        if($query->num_rows() > 0) {
-            $user = $query->next_row();
-            $lastLogin = new DateTime($user->last_login);
-            $now = new DateTime();
-            if($now->format("y") == $lastLogin->format("y") && $now->format("d") == $lastLogin->format("d") && $now->format("d") == $now->format("d")) {
-                return 3;
-            } else {
-                return 2;
-            }
-        } else {
-            return 1;
+    function query_customer_list($startDate,$endDate,$province,$city,$page_index,$page_size){
+        if(isset($startDate) && isset($endDate)){
+            $this->db->where("created_at<",$endDate);
+            $this->db->where("created_at>",$startDate);
         }
-    }
-
-    function confirm_user($openId, $province, $city, $store, $name, $phone, $password) {
-        log_message("info","confirm user information,opendId:".$openId.",province:".$province.",city:".$city.",store:".$store.",name".$name.",phone:".$phone.",password:".$password);
-        $query = $this->db->query("SELECT pi.id FROM lp_promotion_info pi INNER JOIN lp_global_store gs ON pi.store_id = gs.store_id WHERE gs.province = '$province' AND gs.city = '$city' AND gs.store_name = '$store' AND pi.name='$name' AND pi.phone='$phone'");
-        if($query->num_rows()) {
-            $user = $query->next_row();
-            $userId = $user->id;
-            $this->db->query("UPDATE lp_promotion_info SET password='$password', wechat_id='$openId', status = 1 WHERE id = $userId");
-            return true;
-        } else {
-            return false;
+        if(isset($province)){
+            $this->db->where("province",$province);
         }
-    }
-
-    function confirm_change($openId) {
-        $this->db->query("UPDATE lp_promotion_info SET status = 1 WHERE wechat_id = '$openId'");
-    }
-
-    function signin($openId, $password) {
-        $query = $this->db->query("SELECT pi.status, gs.province, gs.city, gs.store_name, pi.phone FROM lp_promotion_info pi INNER JOIN lp_global_store gs ON pi.store_id = gs.store_id WHERE pi.wechat_id = '$openId' AND pi.password = '$password'");
-        if($query->num_rows() > 0) {
-            $user = $query->next_row();
-            $result = array("success" => true);
-            if($user->status == 2) {
-                $result["data"] = $user;
-            }
-            $current_date = date("Y-m-d",time());
-            $this->db->query("update lp_promotion_info set last_login = '$current_date' WHERE wechat_id = '$openId'");
-            return $result;
-        } else {
-            return array("success" => false, "error" => "Password not correct!");
+        if(isset($city)){
+            $this->db->where("city",$city);
         }
+        $this->db->limit($page_index,$page_size);
+        $this->db->select('name','province','city','region','address','birthday','phone','email','wechat_id');
+        $this->db->from('lp_customer_info');
+        return $this->db->get()->result_array();
     }
 
-    function get_user_by_openid($openId) {
-        $query = $this->db->query("SELECT * FROM lp_promotion_info WHERE wechat_id = '$openId'");
-        $user = null;
-        if($query->num_rows() > 0) {
-            $user = $query->next_row();
-        }
-        return $user;
-    }
+
 } 
