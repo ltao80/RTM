10dffb1b52ea2c48542ebef52dc5eae03925e02b
zhengkun <zhengkun@parllay.cn>
2015-05-29 17:30:07 +0800

    add pg code change

--- application/views/admin/edit_user.php
+++ application/views/admin/edit_user.php
@@ -60,7 +60,15 @@
 								<div class="control-group">
 									<label class="control-label my_color_red">请输入PG登录密码：</label>
 									<div class="controls">
-										<input type="text" name="password" class="span6 m-wrap" placeholder="PG登录密码" />
+										<input type="password" name="password" class="span6 m-wrap" placeholder="登录密码" />
+										<span class="help-inline"></span>
+									</div>
+								</div>
+								<div class="control-group">
+									<label class="control-label my_color_red">请再次输入PG登录密码：</label>
+									<div class="controls">
+										<input type="password" name="password_again" class="span6 m-wrap"
+										placeholder="重复密码" />
 										<span class="help-inline"></span>
 									</div>
 								</div>
@@ -138,6 +146,11 @@
 						minlength: 6,
 						required: true
 					},
+					password_again:{
+						minlength: 6,
+						required: true,
+						equalTo:'[name=password]'
+					},
 					email: {
 						required: true,
 						email: true,
@@ -161,6 +174,11 @@
 					password:{
 						minlength: $.validator.format("不能少于 {0} 个字符"),
 						required: "密码不能为空"
+					},
+					password_again:{
+						minlength: $.validator.format("不能少于 {0} 个字符"),
+						required: "密码不能为空",
+						equalTo:"两次输入密码不一致"
 					},
 					email:{
 						required: "邮箱不能为空",
