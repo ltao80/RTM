72f5ab7618c9b04c70025a01afa9a0ffb0b1c7b0
zhengkun <zhengkun@parllay.cn>
2015-05-30 11:04:20 +0800

    router code change

--- application/views/admin/edit_role.php
+++ application/views/admin/edit_role.php
@@ -5,6 +5,8 @@
 <!-- BEGIN HEAD -->
 <head>
     <?php include 'header.php';?>
+    <!--page css-->
+    <link href="media/css/jquery.nestable.css" rel="stylesheet" type="text/css"/>
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
